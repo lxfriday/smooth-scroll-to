0f79d03e0b5ecffaf804d6acaf836fb1baa42180
lxfriday <liu32481184446@outlook.com>
2019-07-16 14:11:37 +0800

    add safe scroll

--- src/index.js
+++ src/index.js
@@ -4,15 +4,24 @@
  * @param pos {number} 滚动到的位置
  * @param rate {number} 速率
  */
-function smoothScrollTo(ref, pos, rate = 2) {
-  if (!ref) {
+function smoothScrollTo(ref = null, pos = 0, rate = 2) {
+  if (rate <= 1) {
+    console.error('rate should be bigger than 1')
     return
   }
+
+  if (!ref) {
+    console.error('ref not exist')
+    return
+  }
+
+  let lastPos = 0
 
   let { scrollTop } = ref
 
   function top() {
     scrollTop = (pos + ref.scrollTop) / rate
+    if (scrollTop === lastPos) return
 
     // 临界判断，终止动画
     if (Math.abs(scrollTop - pos) < 1) {
@@ -20,6 +29,7 @@ function smoothScrollTo(ref, pos, rate = 2) {
       return
     }
     ref.scrollTop = scrollTop
+    lastPos = scrollTop
     requestAnimationFrame(top)
   }
   top()
